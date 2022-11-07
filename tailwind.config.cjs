82bfb05187116d6a877eceeabb33fe33367c3d5c
Christopher Kennedy-Nuñez <chris@onebrightlight.com>
2022-11-07 00:12:55 -0500

    💄 add new color options for cards

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,16 @@ module.exports = {
 		extend: {
 			fontFamily: {
 				body: ['Tofino', 'sans-serif']
-			}
+			},
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+			keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-10deg)' },
+          '50%': { transform: 'rotate(10deg)' },
+        }
+      }
 		},
 	},
 	plugins: [
